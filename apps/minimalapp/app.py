fcdedc7913f225b58afcadb353323ea0f656adba
unho.chang <capcock@snu.ac.kr>
2024-05-31 18:42:55 +0900

    이메일 송부를 위한 설정 및 테스트 완료

--- apps/minimalapp/app.py
+++ apps/minimalapp/app.py
@@ -1,6 +1,8 @@
 import logging
+import os
 from email_validator import validate_email, EmailNotValidError
 from flask_debugtoolbar import DebugToolbarExtension
+from flask_mail import Mail, Message
 from flask import (
     Flask,
     render_template,
@@ -10,15 +12,26 @@ from flask import (
     request,
     redirect,
     flash,
+    make_response,
+    session,
 )
 
 app = Flask(__name__)
 
 app.config["SECRET_KEY"] = "1234qwer"
+
 app.logger.setLevel(logging.DEBUG)
 app.config["DEBUG_TB_INTERCEPT_REDIRECTS"] = False
 
+app.config["MAIL_SERVER"] = os.environ.get("MAIL_SERVER")
+app.config["MAIL_PORT"] = os.environ.get("MAIL_PORT")
+app.config["MAIL_USE_TLS"] = os.environ.get("MAIL_USE_TLS")
+app.config["MAIL_USERNAME"] = os.environ.get("MAIL_USERNAME")
+app.config["MAIL_PASSWORD"] = os.environ.get("MAIL_PASSWORD")
+app.config["MAIL_DEFAULT_SENDER"] = os.environ.get("MAIL_DEFAULT_SENDER")
+
 toolbar = DebugToolbarExtension(app)
+mail = Mail(app)
 
 
 @app.route("/")
@@ -44,7 +57,13 @@ def show_name(name):
 
 @app.route("/contact")
 def contact():
-    return render_template("contact.html")
+    # 응답객체를 취득한다.
+    response = make_response(render_template("contact.html"))
+    # 쿠키를 설정한다.
+    response.set_cookie("flaskbook key", "flaskbook value")
+    # 세션을 설정한다.
+    session["username"] = "AK"
+    return response
 
 
 @app.route("/contact/complete", methods=["GET", "POST"])
@@ -80,8 +99,23 @@ def contact_complete():
             return redirect(url_for("contact"))
 
         # 이메일을 보낸다. 나중에 구현
+        send_email(
+            email,
+            "문의 감사드립니다.",
+            "contact_mail",
+            username=username,
+            description=description,
+        )
 
         # contact 엔드포인트로 리다이렉트 함.
         return redirect(url_for("contact_complete"))
 
     return render_template("contact_complete.html")
+
+
+def send_email(to, subject, template, **kwargs):
+    # 메일을 송신하는 함수
+    msg = Message(subject, recipients=[to])
+    msg.body = render_template(template + ".txt", **kwargs)
+    msg.html = render_template(template + ".html", **kwargs)
+    mail.send(msg)
