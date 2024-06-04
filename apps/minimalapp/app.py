b9e056d212e832242215e4c806c73751b46f2d0b
unho.chang <capcock@snu.ac.kr>
2024-06-04 18:01:39 +0900

    Blueprint 실습 시작 커밋

--- apps/minimalapp/app.py
+++ apps/minimalapp/app.py
@@ -34,6 +34,19 @@ toolbar = DebugToolbarExtension(app)
 mail = Mail(app)
 
 
+def create_app():
+    # 플라스크 인스턴스 생성
+    app = Flask(__name__)
+
+    # crud 앱으로 부터 views를 import한다.
+    from apps.crud import views as crud_views
+
+    # register_blueprints를 이용해서 views의 crud를 앱에 등록한다.
+    app.register_blueprint(crud_views.crud, url_prefix="/crud")
+
+    return app
+
+
 @app.route("/")
 def index():
     return "Hello Flaskbook!"
