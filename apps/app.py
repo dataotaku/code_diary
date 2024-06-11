96d98b97829b6826305cfdd2fa1867a594fa91a9
unho.chang <capcock@snu.ac.kr>
2024-06-11 18:30:16 +0900

    생애 최초 앱연동 db 생성 코드 커밋

--- apps/app.py
+++ apps/app.py
@@ -1,9 +1,28 @@
+from pathlib import Path
 from flask import Flask
+from flask_migrate import Migrate
+from flask_sqlalchemy import SQLAlchemy
+
+# sqlalchemy를 인스턴스화 함
+db = SQLAlchemy()
 
 
 def create_app():
     # 플라스크 인스턴스 생성
     app = Flask(__name__)
+
+    # 앱의 컨피그 설정을 함
+    app.config.from_mapping(
+        SECRET_KEY="2AZSMss3p5QPbcY2hBsJ",
+        SQLALCHEMY_DATABASE_URI=f"sqlite:///{Path(__file__).parent.parent / 'local.sqlite'}",
+        SQLALCHEMY_TRACK_MODIFICATION=False,
+    )
+
+    # sqlalchemy와 앱을 연계함
+    db.init_app(app)
+
+    # migrate와 앱을 연계함
+    Migrate(app, db)
 
     # crud 앱으로 부터 views를 import한다.
     from apps.crud import views as crud_views
